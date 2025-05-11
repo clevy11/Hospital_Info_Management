30bc1f1ba9865c0d2ce6162c9516fb19292b2620
clb <buntulevycaleb@gmail.com>
2025-05-11 03:23:26 +0200

    corrected erros in service and controller

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,15 @@
 module com.example.hospital_info_system {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
 
     opens com.example.hospital_info_system to javafx.fxml;
+    opens com.example.hospital_info_system.model to javafx.base, javafx.fxml;
     exports com.example.hospital_info_system;
+    exports com.example.hospital_info_system.controller;
+    opens com.example.hospital_info_system.controller to javafx.fxml;
+    exports com.example.hospital_info_system.model;
 }
